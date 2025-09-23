9bfc8986ab890b26f09ac55fa73b09e9d6008742
Hagrid <indrajitghosh912@gmail.com>
2025-09-23 11:11:23 +0530

    homepage modified with proper informations and stylish design using bootstrap

--- app/home/home.py
+++ app/home/home.py
@@ -1,3 +1,4 @@
+
 from flask import Blueprint, render_template
 from datetime import datetime
 
@@ -6,3 +7,7 @@ home_bp = Blueprint("home", __name__)
 @home_bp.route("/", methods=["GET"])
 def homepage():
     return render_template("homepage.html", year=datetime.now().year)
+
+@home_bp.route("/docs", methods=["GET"])
+def full_docs():
+    return render_template("full_docs.html", year=datetime.now().year)
